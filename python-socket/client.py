cea3fc9eb9534a0cf856b6fbde61b03f15396b08
Jacob Nishimura <jacob-nishimura@uiowa.edu>
2021-04-30 23:00:04 -0500

    Create simple depth capture example with Record3D and integrate with SocketIO

--- python-socket/client.py
+++ python-socket/client.py
@@ -1,16 +1,78 @@
+import numpy as np
+from record3d import Record3DStream
+import cv2
+from threading import Event
+
 import socketio
 import time
 
-sio = socketio.Client()
-data = []
 
-for i in range(10):
-    sio.connect('http://localhost:3000')
-    time.sleep(1)
+class DepthStreamer:
+    def __init__(self):
+        self.event = Event()
+        self.session = None
+        self.sio = socketio.Client()
+        self.sio.connect('http://localhost:3000')
 
-    data.append(i)
-    sio.emit('achoo', {'message': data})
-    time.sleep(1)
+        @self.sio.on('achooResponse')
+        def on_response(data):
+            print(data)
 
-    sio.disconnect()
-    time.sleep(1)
+    def on_new_frame(self):
+        """
+        This method is called from non-main thread, therefore cannot be used for presenting UI.
+        """
+        self.event.set()  # Notify the main thread to stop waiting and process new frame.
+
+    def on_stream_stopped(self):
+        self.sio.disconnect()
+        print('Stream stopped')
+        exit()
+
+    def connect_to_device(self, dev_idx):
+        print('Searching for devices')
+        devs = Record3DStream.get_connected_devices()
+        print('{} device(s) found'.format(len(devs)))
+        for dev in devs:
+            print('\tID: {}\n\tUDID: {}\n'.format(dev.product_id, dev.udid))
+
+        if len(devs) <= dev_idx:
+            raise RuntimeError('Cannot connect to device #{}, try different index.'
+                               .format(dev_idx))
+
+        dev = devs[dev_idx]
+        self.session = Record3DStream()
+        self.session.on_new_frame = self.on_new_frame
+        self.session.on_stream_stopped = self.on_stream_stopped
+        self.session.connect(dev)  # Initiate connection and start capturing
+
+    def start_processing_stream(self):
+        while True:
+            self.event.wait()  # Wait for new frame to arrive
+
+            # Copy the newly arrived RGBD frame
+            depth = self.session.get_depth_frame()
+            rgb = self.session.get_rgb_frame()
+            # You can now e.g. create point cloud by projecting the depth map using the intrinsic matrix.
+
+            # Postprocess it
+            are_truedepth_camera_data_being_streamed = depth.shape[0] == 640
+            if are_truedepth_camera_data_being_streamed:
+                depth = cv2.flip(depth, 1)
+                rgb = cv2.flip(rgb, 1)
+
+            rgb = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
+
+            self.sio.emit('achoo', {'message': 'achoo!'})
+
+            # Show the RGBD Stream
+            cv2.imshow('RGB', rgb)
+            cv2.imshow('Depth', depth)
+            cv2.waitKey(1)
+
+            self.event.clear()
+    
+if __name__ == '__main__':
+    app = DepthStreamer()
+    app.connect_to_device(dev_idx=0)
+    app.start_processing_stream()
